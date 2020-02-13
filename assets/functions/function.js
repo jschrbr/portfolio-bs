7fc6a1805a0896f415065bd4cbd93e8dffd5f4c5
jschrbr <schrbrr@gmail.com>
2020-02-14 01:09:25 +1100

    Slide, dont push!

--- assets/functions/function.js
+++ assets/functions/function.js
@@ -5,12 +5,10 @@ function toggleNav() {
   if (state == 0) {
     document.getElementById("mySidenav").style.width = "200px";
     document.getElementById("main").style.marginLeft = "200px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
     state = 1;
   } else {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
     state = 0;
   }
 }
